e51e84665ce86fdd6796cab171d25e85fa17a945
Colton Christopher <2tontide@gmail.com>
2022-10-17 11:44:06 -0500

    Variable

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Ralph";
+            int dogAge = 10;
+            char nickName = 'R';
+            bool lovesHiking = true;
+            double tennisBalls = 32;
+            decimal weight = 70.3m;
+            Console.WriteLine($"My dog's name is {dogName} he is {dogAge}, years old and he has {tennisBalls} tennis balls and it is {lovesHiking} that he loves to hike, which keeps him at his weight of {weight} and sometimes we call him by his nickname {nickName}");
         }
     }
 }
